6f8a798eb9df09ee82098d73786946111a467097
Lucky Okoedion <luckyokoedion2000@gmail.com>
2022-01-23 13:38:08 +0100

    Modelling of expected data completed

---
+++ src/app/models/CategoryOptionsEnum.ts
@@ -0,0 +1,13 @@
+export enum CategoryOptionsEnum {
+    business = "business",
+    entertainment = "entertainment",
+    environment = "environment",
+    food = "food",
+    health = "health",
+    politics = "politics",
+    science = "science",
+    sports = "sports",
+    technology = "technology",
+    top = "top",
+    world = "world"
+}
---
+++ src/app/models/INewsItem.ts
@@ -0,0 +1,12 @@
+export interface INewsItem {
+    title?: string,
+    link?: string,
+    keywords?: string,
+    creator?: string[],
+    video_url?: string,
+    description?: string,
+    content?: string,
+    pubDate?: string,
+    image_url?: string,
+    source_id?: string
+}
