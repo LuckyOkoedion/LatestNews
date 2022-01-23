3a4e4a64193f044cf24a90e1f40a2d2bf9d15ba7
Lucky Okoedion <luckyokoedion2000@gmail.com>
2022-01-23 14:57:12 +0100

    Single service for pulling data from the api and filtering has been completed

--- src/app/services/app-state.service.ts
+++ src/app/services/app-state.service.ts
@@ -1,9 +1,67 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { IApiResponse } from '../models/IApiResponse';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppStateService {
 
-  constructor() { }
+  baseApiUrl = "https://newsdata.io/api/1/news";
+  apiKey = environment.API_KEY;
+
+  reqOptions = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    responseType: "json" as const,
+    observe: "response" as const
+  };
+
+  newsList: Subject<IApiResponse> = new Subject();
+
+  constructor(private http: HttpClient) {
+    // console.log(`Api key is: ${environment.API_KEY}`);
+
+  }
+
+
+  callApi(country?: string, category?: string, language?: string) {
+    let constructedUrl = this.baseApiUrl + `?apikey=${this.apiKey}`;
+    // construct url based on available parameters.
+
+    if(typeof country !== 'undefined') {
+      constructedUrl = constructedUrl + `&country=${country}`;
+    }
+
+    if(typeof category !== 'undefined') {
+      constructedUrl = constructedUrl + `&category=${category}`;
+    }
+
+    if(typeof language !== 'undefined') {
+      constructedUrl = constructedUrl + `&language=${language}`;
+    }
+
+    this.http.get<IApiResponse>
+      (constructedUrl, this.reqOptions)
+      .pipe(
+        map((value: HttpResponse<IApiResponse>) => {
+          const theValue = value.body
+          return theValue;
+        })
+      ).subscribe(valu => {
+        const result = valu;
+        if (typeof result !== "undefined") {
+          this.newsList.next(result);
+        }
+      })
+  }
+
+  getNewsList(): Observable<IApiResponse> {
+    return this.newsList.asObservable();
+  }
 }
